468b31a796ca9c744dfcd8dbcce302390ff63ead
EJ Mason <eliasjmason@gmail.com>
2020-03-29 01:03:36 -0700

    Minify html on prod

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,13 @@
 const anchor = require("markdown-it-anchor");
+const htmlmin = require("html-minifier");
 const mdIt = require("markdown-it")({
   html: true
 });
 mdIt.use(anchor);
 
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+const { ELEVENTY_ENV } = process.env;
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/css": "css" });
@@ -13,6 +16,22 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   eleventyConfig.setLibrary("md", mdIt);
+
+  if (ELEVENTY_ENV === "prod") {
+    eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+      if (outputPath.endsWith(".html")) {
+        let minified = htmlmin.minify(content, {
+          useShortDoctype: true,
+          removeComments: true,
+          collapseWhitespace: true
+        });
+        return minified;
+      }
+
+      return content;
+    });
+  }
+
   return {
     dir: {
       input: "src/site",
