6e2c44093efec4ad944d988188769f66bbb3bf65
EJ Mason <eliasjmason@gmail.com>
2020-04-30 15:25:52 -0700

    Add comments to config

--- .eleventy.js
+++ .eleventy.js
@@ -3,10 +3,20 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
-let markdownItLibrary = markdownIt({
+let markdownItConfig = markdownIt({
   html: true
 }).use(markdownItAnchor);
 
+/**
+ * Create a new Nunjucks environment that sets Nunjucks' base path
+ * to "src/templates" so that we can use shorter paths to reference
+ * templates from within each other.
+ *
+ * For example, this allows us to write:
+ *   {% include 'components/navbar.njk' %}
+ * instead of
+ *   {% include 'src/templates/components/navbar.njk' %}
+ */
 let Nunjucks = require("nunjucks");
 let nunjucksEnvironment = new Nunjucks.Environment(
   new Nunjucks.FileSystemLoader("src/templates")
@@ -19,9 +29,13 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-  eleventyConfig.setLibrary("md", markdownItLibrary);
+  eleventyConfig.setLibrary("md", markdownItConfig);
   eleventyConfig.setLibrary("njk", nunjucksEnvironment);
 
+  /**
+   * If we're in prod, we want to minify all HTML as we process it
+   * @see: https://www.11ty.dev/docs/config/#transforms-example-minify-html-output
+   */
   if (ELEVENTY_ENV === "prod") {
     eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
       if (outputPath.endsWith(".html")) {
