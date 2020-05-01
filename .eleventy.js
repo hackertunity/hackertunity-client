ce82c9ac5b6cf57797e655fdf0ddaaf5654bdadb
EJ Mason <eliasjmason@gmail.com>
2020-04-30 17:41:02 -0700

    Add filters

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+const { DateTime } = require("luxon");
 const htmlmin = require("html-minifier");
 const pluginNavigator = require("@11ty/eleventy-navigation");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
@@ -26,6 +27,26 @@ let nunjucksEnvironment = new Nunjucks.Environment(
 const { ELEVENTY_ENV } = process.env;
 
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLL yyyy"
+    );
+  });
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter("htmlDateString", dateObj => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  });
+
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter("head", (array, n) => {
+    if (n < 0) {
+      return array.slice(n);
+    }
+
+    return array.slice(0, n);
+  });
+
   eleventyConfig.addPassthroughCopy("src/assets");
 
   eleventyConfig.addPlugin(pluginNavigator);
