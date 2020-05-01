c391857dfa7747bc7e0785eae57ef8f975f8f699
EJ Mason <eliasjmason@gmail.com>
2020-04-30 18:14:46 -0700

    remove dummy passthroughcopy

--- .eleventy.js
+++ .eleventy.js
@@ -48,7 +48,6 @@ module.exports = function(eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy("src/assets");
-  eleventyConfig.addPassthroughCopy("foo");
 
   eleventyConfig.addPlugin(pluginNavigator);
   eleventyConfig.addPlugin(pluginRss);
