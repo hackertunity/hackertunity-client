4a9281b18a6786d47447ef19e46c64a674c412d8
Leo Yockey <leoyockey@gmail.com>
2020-05-28 13:47:51 -0700

    remove Vue

--- src/assets/js/navbar.js
+++ src/assets/js/navbar.js
@@ -1,32 +1,34 @@
-const navBarVM = new Vue({
-  delimiters: ["${", "}"],
-  el: "#vue-nav-menu",
-  data: {
-    label: "Nav",
-    inst: false
-  },
-  methods: {
-    handleClick(e) {
-      // TODO: close any  menus already open
-      const node = e.target;
-      if (node.parentNode.className == "has-submenu") {
-        node.parentNode.className = "has-submenu open";
-        node.setAttribute('aria-expanded', "true");
-      } else {
-        node.parentNode.className = "has-submenu";
-        node.setAttribute('aria-expanded', "false");
-      }
-    },
-    logJson(o) {
-      console.log(JSON.stringify(o))
-    },
-    startHover(e) {
-      e.target.className = "has-submenu open";
-      e.target.setAttribute('aria-expanded', "true");
-    },
-    endHover(e) {
-      e.target.className = "has-submenu";
-      e.target.setAttribute('aria-expanded', "false");
-    }
+const handleClick = (e) => {
+  // TODO: close any  menus already open
+  const node = e.target;
+  if (node.parentNode.className == "has-submenu") {
+    node.parentNode.className = "has-submenu open";
+    node.setAttribute('aria-expanded', "true");
+  } else {
+    node.parentNode.className = "has-submenu";
+    node.setAttribute('aria-expanded', "false");
   }
-});
+}
+
+const startHover = (e) => {
+  e.target.className = "has-submenu open";
+  e.target.setAttribute('aria-expanded', "true");
+}
+
+const endHover = (e) => {
+  e.target.className = "has-submenu";
+  e.target.setAttribute('aria-expanded', "false");
+}
+
+const setupListeners = (className, eventType, callback) => {
+  const elements = document.getElementsByClassName(className);
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+const init = () => {
+  setupListeners('title-button', 'click', handleClick);
+}
+
+window.addEventListener('DOMContentLoaded', init);
