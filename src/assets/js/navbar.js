445cf5bb27d1090c15a20b0591db9159da913046
EJ Mason <eliasjmason@gmail.com>
2020-05-28 20:08:01 -0700

    Delete hover handlers

--- src/assets/js/navbar.js
+++ src/assets/js/navbar.js
@@ -1,22 +1,12 @@
 const handleClick = (e) => {
+  console.log('handling');
   const node = e.target;
   if (node.parentNode.className == 'has-submenu') {
     closeMenus();
     node.parentNode.className = 'has-submenu open';
     node.setAttribute('aria-expanded', 'true');
   } else closeMenus();
-}
-
-// TODO: get hover working
-const startHover = (e) => {
-  e.target.className = 'has-submenu open';
-  e.target.setAttribute('aria-expanded', 'true');
-}
-
-const endHover = (e) => {
-  e.target.className = 'has-submenu';
-  e.target.setAttribute('aria-expanded', 'false');
-}
+};
 
 const closeMenus = () => {
   const element = document.getElementsByClassName('has-submenu open')[0];
@@ -24,17 +14,17 @@ const closeMenus = () => {
     element.className = 'has-submenu';
     element.children[0].setAttribute('aria-expanded', 'false');
   }
-}
+};
 
 const setupListeners = (className, eventType, callback) => {
   const elements = document.getElementsByClassName(className);
   for (let i = 0; i < elements.length; i++) {
     elements[i].addEventListener(eventType, callback);
   }
-}
+};
 
 const init = () => {
   setupListeners('title-button', 'click', handleClick);
-}
+};
 
 window.addEventListener('DOMContentLoaded', init);
